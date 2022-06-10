2521677ae58b8b12e6106c6561c334381e7d8d76
Abram <58718191+heyuhowudoin@users.noreply.github.com>
2022-06-09 17:24:19 -0700

    improved from 50-60% to now 85-95% accurate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy as np, gzip
+import numpy as np, gzip, random
 
 
 def sigmoid(z):
@@ -58,9 +58,11 @@ class layer:
 class neural_network:
 	def __init__(self, n_per_l):
 		self.layers = [layer(inp, nrn) for inp, nrn in zip(n_per_l, n_per_l[1:])]
+		self.num_of_layers = len(n_per_l)
 		print("layers created")
 
 	def forwardprop(self, input_):
+		self.input_ = input_
 		for layer in self.layers:
 			layer.forwardprop(input_)
 			input_ = layer.activations
@@ -71,31 +73,25 @@ class neural_network:
 		cost = (self.layers[-1].activations - target)
 		cost_prime = cost * sigmoid_prime(self.layers[-1].values)
 
-		self.layers[-1].b_adj += cost_prime
+		self.layers[-1].b_adj = cost_prime
 		shaped_cost = np.array([cost_prime]).transpose()
 		shaped_activations = np.array([self.layers[-2].activations])
-		self.layers[-1].w_adj += np.dot(shaped_cost, shaped_activations).transpose()
+		self.layers[-1].w_adj = np.dot(shaped_cost, shaped_activations).transpose()
 
-		for l in range(2, len(self.layers)):
-			layer = self.layers[-l]
-			# print(len(layer.biases), "b")
-			# print(np.array([self.layers[-l+1].weights]), "w")
-			# print(np.array(cost_prime).transpose(), "c")
-			shaped_cost = np.array(cost_prime).transpose()
-			shaped_weights = np.array(self.layers[-l+1].weights)
-			cost_prime = np.dot(shaped_weights, shaped_cost).transpose()
-			layer.b_adj += cost_prime
+		for l in range(2, len(self.layers) + 1):
+			cost_prime = np.dot(self.layers[-l + 1].weights, cost_prime) * sigmoid_prime(self.layers[-l].values)
 
-			shaped_cost = np.array([cost_prime]).transpose()
-			shaped_activations = np.array([self.layers[-l-1].activations])
-			layer.w_adj += np.dot(shaped_cost, shaped_activations).transpose()
+			self.layers[-l].b_adj += cost_prime
+			self.layers[-l].w_adj += np.dot(np.array([cost_prime]).transpose(), np.array([self.input_])).transpose()
+
 
 	def train(self, lrn_rt, batch_size, epochs, ep_size, inputs):
 		train_data, test_i, test_l = inputs
-		mini_batches = [train_data[k:k + batch_size] for k in range(0, len(train_data), batch_size)]
 		print("mini_batches shaped")
 
 		for ep in range(epochs):
+			random.shuffle(train_data)
+			mini_batches = [train_data[k:k + batch_size] for k in range(0, len(train_data), batch_size)]
 			for mini_batch in mini_batches:
 				for img, lbl in mini_batch:
 					self.forwardprop(np.reshape(img, (784)))
@@ -110,7 +106,7 @@ class neural_network:
 			correct = 0
 			for img, lbl in zip(test_i, test_l):
 				self.forwardprop(np.reshape(img, (784)))
-				if list(self.output).index(max(self.output)) == lbl:
+				if np.argmax(self.output) == lbl:
 					correct += 1
 
 			print("Epoch", ep, ":", correct, "/", len(test_l))
@@ -121,4 +117,5 @@ print("getting data")
 data = get_data()
 print("data collected")
 print("starting training")
-net.train(0.1, 30, 30, 60000, data)
+net.train(8, 30, 30, 60000, data)
+
